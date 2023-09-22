4aa84fd29c276b09093813215b42a79e2f973ad5
oussama ennadafy <oussama.ennadafy@gmail.com>
2023-09-22 15:02:45 +0100

    fix some dashboard links

--- resources/views/vendor/dashboard/layouts/menu.blade.php
+++ resources/views/vendor/dashboard/layouts/menu.blade.php
@@ -1,6 +1,7 @@
 <div class="wsus__dashboard_menu">
     <div class="wsusd__dashboard_user">
-        <img src="{{ asset('frontend/images/dashboard_user.jpg') }}" alt="img" class="img-fluid">
+        <img src="{{ Auth::user()->image ? asset(Auth::user()->image) : asset('frontend/images/default_profile_picture.jpeg') }}"
+            alt="img" class="img-fluid">
         <p>{{ Auth::user()->name }}</p>
     </div>
 </div>
