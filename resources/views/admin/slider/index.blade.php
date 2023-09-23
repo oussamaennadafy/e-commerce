cdb2b3a9068b595987d961aea8fbbec6e18e1759
oussama <oussama.ennadafy@gmail.com>
2023-09-23 13:42:11 +0100

    update composer

--- resources/views/admin/slider/index.blade.php
+++ resources/views/admin/slider/index.blade.php
@@ -5,7 +5,7 @@
     <!-- Main Content -->
     <section class="section">
         <div class="section-header">
-            <h1>Table</h1>
+            <h1>Slider</h1>
             <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                 <div class="breadcrumb-item"><a href="#">Components</a></div>
@@ -14,8 +14,8 @@
         </div>
 
         <div class="section-body">
-            <h2 class="section-title">Table</h2>
-            <p class="section-lead">Example of some Bootstrap table components.</p>
+            <h2 class="section-title">Hi there!</h2>
+            <p class="section-lead">here you can see all the sliders</p>
 
 
             <div class="row">
@@ -28,6 +28,7 @@
                             </div>
                         </div>
                         <div class="card-body">
+                            {{-- {{ $dataTable->table() }} --}}
                         </div>
                     </div>
                 </div>
