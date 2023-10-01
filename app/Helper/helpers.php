846a57449d098f6a474953119f028b48978b09b1
oussama ennadafy <oussama.ennadafy@gmail.com>
2023-10-01 13:52:14 +0100

    add helper function for the admin sidebar

---
+++ app/Helper/helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+ * set sidebar item active
+ */
+
+function setActive(array $route)
+{
+    if (is_array($route)) {
+        foreach ($route as $r) {
+            if (request()->routeIs($r)) {
+                return 'active';
+            }
+        }
+    }
+}
